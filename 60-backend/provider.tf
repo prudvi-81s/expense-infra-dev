701e9fa3ede3479a99ddc19d0530b975ed76fc5e
Prudviraj <prudvivemula10@mail.com>
2024-10-09 07:34:04 +0530

    terraform

--- 60-backend/provider.tf
+++ 60-backend/provider.tf
@@ -1,19 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "5.66.0"
     }
   }
 
   backend "s3" {
-    bucket         = "81p-remote-state"
-    key            = "expense-dev-backend"
-    region         = "us-east-1"
+    bucket = "81s-remote-state"
+    key    = "expense-dev-backend"
+    region = "us-east-1"
     dynamodb_table = "81s-locking"
   }
 }
-
 
 provider "aws" {
   # Configuration options
